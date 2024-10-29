d78c7591c44973eb9ab8dfb54ef49f7ce4283ac7
Saiyam Yadav <ydvsaiyam001@gmail.com>
2024-10-29 19:12:59 +0530

    Cover basics and primitive parts of JS

---
+++ 01. Beginners/02. Primitives/01 Variables/live coding/app.js
@@ -0,0 +1,8 @@
+// How to make variables ?
+// (reserved keyword): (variable name): (type of value)
+
+let variable; // undefined
+let name = "HuXn WebDev";
+console.log(name);
+const channelName = "WebDev Academy";
+console.log(channelName);
