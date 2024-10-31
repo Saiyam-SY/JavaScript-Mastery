a9516bf91743befbe90dbe82bc62639e4362a658
Saiyam Yadav <ydvsaiyam001@gmail.com>
2024-10-31 17:22:41 +0530

    Template literals (Intermediate Start)

---
+++ 02. Intermediate/01. Template String/exercise/app.js
@@ -0,0 +1,21 @@
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+let text = `The quick
+brown fox
+jumps over
+the lazy dog`;
+
+// ----------------
+let firstName = "Saiyam";
+let lastName = "WebDev";
+
+let text2 = `Welcome, ${firstName} ${lastName}!`;
+console.log(text2);
+// ----------------
